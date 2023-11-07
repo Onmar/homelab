a7d6dc4e053237dcb8c1194e714121da65349d6e
Marcel Burri <mburri@mburri.ch>
2023-11-07 22:08:44 +0100

    00-controller

---
+++ 00-controller/README.md
@@ -0,0 +1,7 @@
+# Ansible Controller Setup
+
+## Install Ansible
+* [https://docs.ansible.com/ansible/latest/installation_guide/installation_distros.html#installing-ansible-on-ubuntu]()
+
+## Install YubiKey Software
+* [https://github.com/drduh/YubiKey-Guide]()
---
+++ 00-controller/install-yubikey-tools.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#sudo apt -y install wget gnupg2 gnupg-agent dirmngr cryptsetup scdaemon pcscd secure-delete hopenpgp-tools yubikey-personalization
+
+
+cd ~/.gnupg ; wget https://raw.githubusercontent.com/drduh/config/master/gpg.conf
+chmod 600 gpg.conf
+
+sudo apt update && sudo apt install -y gnupg2 gnupg-agent gnupg-curl scdaemon pcscd
+
+echo "Edit ~/.gnupg/gpg.conf and enable keyserver url"
+read -p "Press any key to continue..." -n1 -s
+nano ~/.gnupg/gpg.conf
+
+
+cd ~/.gnupg ; wget https://raw.githubusercontent.com/drduh/config/master/gpg-agent.conf
+chmod 600 gpg-agent.conf
+
+
+cat << EOF >> ~/.bashrc
+export GPG_TTY="$(tty)"
+export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
+gpgconf --launch gpg-agent
+EOF
