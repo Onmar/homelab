87a7d0321ed74c3db7a2e2ecd3f1027d368fba14
Marcel Burri <mburri@mburri.ch>
2023-11-20 14:57:15 +0100

    Added comissioning

--- 00-controller/01-install-ansible.sh
+++ 00-controller/01-install-ansible.sh
@@ -3,4 +3,5 @@ sudo apt update
 sudo apt install --yes software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install --yes ansible
-
+sudo apt install --yes python3-pip
+pip3 install passlib
