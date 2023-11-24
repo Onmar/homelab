648037a799207f968eb191216174e41d4100febf
Marcel Burri <mburri@mburri.ch>
2023-11-24 13:33:54 +0100

    lint

--- 00-controller/01-install-ansible.sh
+++ 00-controller/01-install-ansible.sh
@@ -4,4 +4,4 @@ sudo apt install --yes software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install --yes ansible
 sudo apt install --yes python3-pip
-pip3 install passlib
+pip3 install passlib ansible-lint
--- 10-titan/10-ansible/base-install-debian.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-apt-get install --yes openssh-server
-sed -i -E 's/^#?PermitRootLogin.*$/PermitRootLogin yes/' /etc/ssh/sshd_config
-systemctl restart sshd.service
